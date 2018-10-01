8ce09c2e9d759f933a870019d33df6c3a83b9288
Gina Fedder <ginafedder@yahoo.com>
2018-10-01 19:16:45 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,6 +39,13 @@ class Song
   
   def self.artist_count
     artist_count = {}
-    artist_count[@artist] = @@artists.count
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1 
+      else
+        artist_count[artist] = 1 
+      end
+    end
+    artist_count
   end 
 end 
