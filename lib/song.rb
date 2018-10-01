9b8b343cfb31a3af532b7d6294cddc33a3991a94
Gina Fedder <ginafedder@yahoo.com>
2018-10-01 18:46:39 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,8 +2,9 @@ def Song
   attr_accessor :name, :artist, :genre 
   @@count = 0
   @@genres = []
-  
+  @@artist = []
   def initialize(name, artist, genre)
+    @@artist << artist
     @@genre << genre
     @@count += 1 
   end 
@@ -15,4 +16,8 @@ def Song
   def self.genres 
     
   end
+  
+  def self.artists
+    
+  end 
 end 
